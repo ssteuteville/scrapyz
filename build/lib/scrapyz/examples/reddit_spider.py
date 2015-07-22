c1c7a4d3940b44fe54883f9f3809f06943c75400
ssteuteville <ssteuteville@gmail.com>
2015-07-22 16:32:53 -0700

    pypi build

---
+++ build/lib/scrapyz/examples/reddit_spider.py
@@ -0,0 +1,52 @@
+from scrapyz.core import *
+from scrapyz.util import absolute_url
+
+
+def join(value, response):
+    if isinstance(value, (list, tuple)):
+        value = " ".join(value)
+    return value
+
+
+class RedditSpider(GenericSpider):
+    name = "reddit"
+    start_urls = ["https://www.reddit.com/"]
+
+    class Meta:
+        items = ".thing"
+        targets = [
+            CssTarget("rank", ".rank::text"),
+            CssTarget("upvoted", ".upvoted::text"),
+            CssTarget("dislikes", ".dislikes::text"),
+            CssTarget("likes", ".likes::text"),
+            CssTarget("title", "a.title::text"),
+            CssTarget("domain", ".domain > a::text"),
+            CssTarget("datetime", ".tagline > time::attr(datetime)"),
+            CssTarget("author", ".tagline > .author::text"),
+            CssTarget("subreddit", ".tagline > .subreddit::text"),
+            CssTarget("comments", ".comments::text")
+        ]
+
+
+class RedditSpider2(IndexDetailSpider):
+    name = "reddit2"
+    start_urls = ["https://www.reddit.com/"]
+
+    class Meta:
+        detail_path = CssTarget("detail_path", ".title > a::attr(href)", [absolute_url])
+        detail_targets = [
+            CssTarget("content", ".usertext-body > div > p::text", [join]),
+        ]
+        items = ".thing"
+        targets = [
+            CssTarget("rank", ".rank::text"),
+            CssTarget("upvoted", ".upvoted::text"),
+            CssTarget("dislikes", ".dislikes::text"),
+            CssTarget("likes", ".likes::text"),
+            CssTarget("title", "a.title::text"),
+            CssTarget("domain", ".domain > a::text"),
+            CssTarget("datetime", ".tagline > time::attr(datetime)"),
+            CssTarget("author", ".tagline > .author::text"),
+            CssTarget("subreddit", ".tagline > .subreddit::text"),
+            CssTarget("comments", ".comments::text")
+        ]
---
+++ build/lib/scrapyz/util.py
@@ -0,0 +1,21 @@
+from scrapy import Item
+from scrapy.http.request import Request
+from scrapy.utils.response import get_base_url
+import urlparse
+
+
+def gen_item(fields):
+    return type("GenericItem", (Item,), fields)
+
+
+def gen_request(url, callback, item=None):
+    r = Request(url, callback=callback)
+    if item:
+        r.meta['item'] = item
+    return r
+
+
+def absolute_url(link, response):
+    if isinstance(link, list):
+        link = link[0] if len(link) else ""
+    return urlparse.urljoin(get_base_url(response), link)
